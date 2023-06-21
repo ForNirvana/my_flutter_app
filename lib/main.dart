fbb100bdb90ed059a81e61752d5e8ff5cebc0d18
ForNirvana qh <forgamorwork@163.com>
2023-06-21 15:02:34 +0000

    save

--- lib/main.dart
+++ lib/main.dart
@@ -34,6 +34,12 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
+  void _subtractCounter() {
+    setState(() {
+      _counter--;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -44,15 +50,80 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text('You have pushed the button this many times:'),
+            Wrap(
+              spacing: 8.0, // 主轴(水平)方向间距
+              runSpacing: 4.0, // 纵轴（垂直）方向间距
+              alignment: WrapAlignment.center, //沿主轴方向居中
+              children: <Widget>[
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.yellow, child: Text('A')),
+                  label: Text('TextStyle'),
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.yellow, child: Text('B')),
+                  label: Text('Wrap'),
+                ),
+                Chip(
+                  avatar: CircleAvatar(
+                      backgroundColor: Colors.yellow, child: Text('C')),
+                  label: Text('DecoratedBox'),
+                ),
+              ],
+            ),
+            Text(
+              "You have pushed the button this many times:",
+              textScaleFactor: 1.5,
+            ),
             Text(
               '$_counter',
               style: Theme.of(context).textTheme.headline4,
+            ),
+            TextButton.icon(
+              icon: Icon(Icons.remove),
+              label: Text("减"),
+              onPressed: _subtractCounter,
+            ),
+            DecoratedBox(
+                decoration: BoxDecoration(
+                    gradient: LinearGradient(
+                        colors: [Colors.red, Colors.orange.shade700]), //背景渐变
+                    borderRadius: BorderRadius.circular(3.0), //3像素圆角
+                    boxShadow: [
+                      //阴影
+                      BoxShadow(
+                          color: Colors.black54,
+                          offset: Offset(2.0, 2.0),
+                          blurRadius: 4.0)
+                    ]),
+                child: Padding(
+                  padding:
+                      EdgeInsets.symmetric(horizontal: 80.0, vertical: 18.0),
+                  child: Text(
+                    "Login",
+                    style: TextStyle(color: Colors.white),
+                  ),
+                )),
+            Text(
+              "The End",
+              style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 81.0,
+                  height: 1.2,
+                  fontFamily: "Courier",
+                  background: Paint()..color = Colors.yellow,
+                  decoration: TextDecoration.underline,
+                  decorationStyle: TextDecorationStyle.dashed),
             ),
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
+        foregroundColor: Colors.blue,
+        elevation: 12,
+        splashColor: Colors.purple,
+        hoverColor: Colors.orange,
         onPressed: _incrementCounter,
         tooltip: 'Increment',
         child: Icon(Icons.add),
